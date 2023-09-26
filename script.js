e10f448c1e5acc401aa0a12b805d443985bb1447
eme-web <udobongeme4@gmail.com>
2023-09-26 11:07:18 +0100

    modified correct answer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const correctAnswer = ["A","A","A","A","A","A","A","A","A","A"];
+const correctAnswer = ["C","A","D","B","A","A","D","C","A","A"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
 const questions = document.querySelectorAll(".question");
